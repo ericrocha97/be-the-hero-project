b21a86aff5c580ac22e338564c44d06cffc73499
Eric Rocha <ericrochaeric@gmail.com>
2020-03-27 15:38:27 -0300

    edit
    edit:
    favicon and host api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://be-the-hero-backend-node-api.herokuapp.com/'
 });
 
 export default api;
