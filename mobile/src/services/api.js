cee39edc1f395941ddd02ba873d0beed66b1d5df
Eric Rocha <ericrochaeric@gmail.com>
2020-03-27 15:58:59 -0300

    alterado host da api

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.104:3333'
+    baseURL: 'https://be-the-hero-backend-node-api.herokuapp.com/'
 });
 
 export default api;
